f4238759be0b742732a561fcbd5d01746317cd97
Drzazgakrz <30790462+Drzazgakrz@users.noreply.github.com>
2018-12-07 01:32:22 +0100

    Add files via upload

---
+++ Db.java
@@ -0,0 +1,76 @@
+import java.sql.*;
+
+public class Db {
+	private static Connection conn;
+	private static Statement statement;
+	public static void main(String[] args){
+		try{
+			conn = DriverManager.getConnection("jdbc:mysql://10.0.10.3:3306/my_database","kdrzazga","pass");
+			statement = conn.createStatement();
+			switch (args[0]){
+				case "insert":
+					Db.insert(args);
+					break;
+				case "select":
+					Db.show();
+					break;
+				case "init":
+					Db.init();
+					break;
+			}
+		} catch (Exception e){
+			e.printStackTrace();	
+		}
+	}
+	public static void insert(String[] strings){
+		try{
+			statement.executeUpdate("INSERT INTO users(id,firstname,lastname) VALUES ("+strings[1]+",'"+strings[2]+"','"+strings[3]+"')");
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public static void show(){
+		try{
+			ResultSet result = statement.executeQuery("SELECT * FROM users");
+			while(result.next()){
+				int i = result.getInt("id");
+				String fn = result.getString("firstname");
+				String ln = result.getString("lastname");
+				System.out.println(Integer.toString(i)+" "+fn+" "+ln);
+			}
+		}catch (SQLException e) {
+				System.out.println("Blad dzialania");
+				e.printStackTrace();
+		}
+	}
+
+	public static void init(){
+		try {
+				Thread.sleep(30000);
+				statement.executeUpdate("CREATE TABLE IF NOT EXISTS users ("+
+					"id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,"+
+					"firstname VARCHAR(30),"+
+					"lastname VARCHAR(30)"+
+					")");
+				statement.executeUpdate("INSERT IGNORE INTO users (id,firstname,lastname) VALUES (1,'Jan', 'Kowalski'),"+
+					"(2,'Adam', 'Nowak'),(3,'Krzysztof', 'Drzazga')");
+				ResultSet result = statement.executeQuery("SELECT * FROM users");
+				while(result.next()){
+					int i = result.getInt("id");
+					String fn = result.getString("firstname");
+					String ln = result.getString("lastname");
+					System.out.println(Integer.toString(i)+" "+fn+" "+ln);
+				}
+				conn.close();
+			} catch (SQLException e) {
+				System.out.println("Blad dzialania");
+				e.printStackTrace();
+			} catch (Exception e) {
+				System.out.println("Blad dzialania");
+				e.printStackTrace();
+				
+			}
+	}
+}
+
---
+++ skrypt.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+java Db init
+while  true ; do
+	sleep 300s
+done
